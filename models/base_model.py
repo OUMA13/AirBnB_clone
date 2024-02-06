55a60793a037efca98db1ae8a2ddcc35c67807d4
Clear008 <wissal-exo@hotmail.com>
2024-02-06 19:58:09 +0100

    correct the output by including __class__

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 """
-
+    this Module support the BaseModel Class as the parent Class for all the Inheritate instance
 """
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """ The base Class for all the inheritate attribute """
+
     def __init__(self, *arg, **kwargs):
         """Inisialization of attribues of the BaseModel Class"""
+        
         isof = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs:
+        
+        if (len(kwargs) == 0):
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for ow_k, ow_v in kwargs.items():
                 if ow_k == 'created_at' or ow_k == 'updated_at':
                     setattr(self, ow_k, datetime.strptime(ow_v, isof))
                 elif ow_k != '__class__':
                     setattr(self, ow_k, ow_v)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
     
     def save(self):
+        """  it updates the instance is attribute updated_at with the current date time """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """ returns a dictionary containing all keys and values of __dict__ 
+        of the instance in the base class including __class__ update_at and create_at"""
         data = {}
         for k, v in self.__dict__.items():
             if k == 'created_at':
@@ -31,6 +38,7 @@ class BaseModel:
             if k == 'updated_at':
                 v = v.isoformat()
             data[k] = v
+        data['__class__'] = self.__class__.__name__
         return data
 
     def __str__(self):
