a27855f09cc31e18e400ef37a7d466ee1b2de5b6
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-02-08 07:54:02 -0800

    amenty

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining the Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Definning a Class that represent an amenity."""
+
+    name = ""
