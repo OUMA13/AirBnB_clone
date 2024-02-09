7043982852aefb398728ea63e90b5e0c24a6ad84
OUMA13 <omaimaelkhanchoufi9@gmail.com>
2024-02-09 14:04:04 -0800

    test city

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains the TestCityDocs classes."""
+
+from models.base_model import BaseModel
+import json
+
+class City(BaseModel):
+    """ Contains the TestCityDocs classes"""
+
+    state_id = ""
+    name = ""
+
+ow_city = City()
+ow_city.name = "casa"
+ow_city.state_id = "MA"
+ow_city.save()
+print(ow_city.to_dict())
+wnw_city = City.from_dict(ow_city.to_dict())
+print(wnw_city.name)
+print(wnw_city.state_id)
