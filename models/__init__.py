6ba7df2f9db1ed880747045a24f185eafd6a532a
Clear008 <wissal-exo@hotmail.com>
2024-02-10 16:30:37 +0100

    testing

--- models/__init__.py
+++ models/__init__.py
@@ -1,23 +1,7 @@
 #!/usr/bin/python3
-"""FileStorage instance for application"""
-from models.base_model import BaseModel
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
-classes = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "Place": Place,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Review": Review
-}
 
 storage = FileStorage()
 storage.reload()
