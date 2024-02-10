da386f647490e876db194744367de925557ea7a4
Clear008 <wissal-exo@hotmail.com>
2024-02-10 11:39:13 +0100

    correct basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -13,21 +13,22 @@ class BaseModel:
     """ The base Class for all the inheritate attribute """
 
     def __init__(self, *args, **kwargs):
-        """Inisialization of attribues of the BaseModel Class"""
-
-        isof = "%Y-%m-%dT%H:%M:%S.%f"
-
+        '''
+            Initialize public instance attributes.
+        '''
         if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            for ow_k, ow_v in kwargs.items():
-                if ow_k == 'created_at' or ow_k == 'updated_at':
-                    setattr(self, ow_k, datetime.strptime(ow_v, isof))
-                elif ow_k != '__class__':
-                    setattr(self, ow_k, ow_v)
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def save(self):
         """
