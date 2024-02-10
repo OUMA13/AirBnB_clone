0f3fb172a984fe777f1855c2d462d74ef0aa4723
Clear008 <wissal-exo@hotmail.com>
2024-02-10 11:30:58 +0100

    correct basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
         isof = "%Y-%m-%dT%H:%M:%S.%f"
 
         if (len(kwargs) == 0):
-            self.id = kwargs.str(uuid.uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
