87c743df0239a29483f010543d7475221b7a37f7
Clear008 <wissal-exo@hotmail.com>
2024-02-10 11:43:37 +0100

    correct basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,62 +1,56 @@
 #!/usr/bin/python3
-'''
-    This module defines the BaseModel class
-'''
+"""
+    This Module support the BaseModel Class as
+    the parent Class for all the Inheritate instance.
+"""
+
+import models
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
-    '''
-        Base class for other classes to be used for the duration.
-    '''
+    """ The base Class for all the inheritate attribute """
+
     def __init__(self, *args, **kwargs):
-        '''
-            Initialize public instance attributes.
-        '''
+        """Inisialization of attribues of the BaseModel Class"""
+
+        isof = "%Y-%m-%dT%H:%M:%S.%f"
+
         if (len(kwargs) == 0):
-            self.id = str(uuid.uuid4())
+            self.id = kwargs.get('id', str(uuid.uuid4()))
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
         else:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
-
-    def __str__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
-
-    def __repr__(self):
-        '''
-            Return string representation of BaseModel class
-        '''
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id, self.__dict__))
+            for ow_k, ow_v in kwargs.items():
+                if ow_k == 'created_at' or ow_k == 'updated_at':
+                    setattr(self, ow_k, datetime.strptime(ow_v, isof))
+                elif ow_k != '__class__':
+                    setattr(self, ow_k, ow_v)
 
     def save(self):
-        '''
-            Update the updated_at attribute with new.
-        '''
+        """
+        it updates the instance is attribute
+        updated_at with the current date time
+        """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        '''
-            Return dictionary representation of BaseModel class.
-        '''
-        cp_dct = dict(self.__dict__)
-        cp_dct['__class__'] = self.__class__.__name__
-        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        """
+        Returns a dictionary containing all keys and values of __dict__
+        of the instance in the base class including
+        __class__ update_at and create_at"""
+        data = {}
+        for k, v in self.__dict__.items():
+            if k == 'created_at':
+                v = v.isoformat()
+            if k == 'updated_at':
+                v = v.isoformat()
+            data[k] = v
+        data['__class__'] = self.__class__.__name__
+        return data
 
-        return (cp_dct)
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
