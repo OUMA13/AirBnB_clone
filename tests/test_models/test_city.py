83d0335dd77269f39d9aeae3b4f4bf68da6d6a59
Clear008 <wissal-exo@hotmail.com>
2024-02-11 01:34:12 +0100

    add mores tests

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,22 +1,51 @@
 #!/usr/bin/python3
-"""Contains the TestCityDocs classes."""
+"""
+this Module define the Unittests( tast different cases) for models/city.py
+"""
 
 from models.base_model import BaseModel
-import json
+from models.city import City
+import unittest
 
 
-class City(BaseModel):
-    """ Contains the TestCityDocs classes"""
+class test_City_Uni(unittest.TestCase):
+    """Contains the TestCity test cases/unit tests classes """
 
-    state_id = ""
-    name = ""
+    def __init__(self, *args, **kwargs):
+        """Preparing the test inisialization """
 
+        super().__init__(*args, **kwargs)
+        self.ow_n = "City"
+        self.ow_v = City
 
-ow_city = City()
-ow_city.name = "casa"
-ow_city.state_id = "MA"
-ow_city.save()
-print(ow_city.to_dict())
-wnw_city = City.from_dict(ow_city.to_dict())
-print(wnw_city.name)
-print(wnw_city.state_id)
+    def test_state_id_isan_instance(self):
+        """
+        test if state id attribute in an instance
+        of the City represent a string
+        """
+        city_instance = self.value()
+        self.assertEqual(type(city_instance.state_id), str)
+
+    def test_name_isan_insatance(self):
+        """
+        test if name attribute in an instance
+        of the City represent a string
+        """
+        city_instance = self.value()
+        self.assertEqual(type(city_instance.name), str)
+
+    def test_name_initial_value(self):
+        """
+        test if state id attribute in an instance of the City
+        is initially an empty string
+        """
+        city_instance = self.value()
+        self.assertEqual(city_instance.name, "")
+
+    def test_state_id_initial_value(self):
+        """
+        test if state id attribute in an instance of the City
+        is initially an empty string
+        """
+        city_instance = self.value()
+        self.assertEqual(city_instance.state_id, "")
